4bd81939803bb3149c44aab4a98e2792d1e43191
Valentin Bellamy <bellamyvalentin@gmail.com>
2019-11-02 17:17:02 +1100

    Map ready to prod

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,9 +6,9 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>Cette page n'existe pas</p>
-    <Link className="btn btn-secondary mbm" to="/">
+    <h1 className="text-black mbs">NOT FOUND</h1>
+    <p className="text-gray mbs">Cette page n'existe pas</p>
+    <Link className="btn btn-secondary" to="/">
       Retour à l'accueil
     </Link>
   </Layout>
